8f83421f7d52790dcd05ef72a857b4d5b2b897ad
Ayan Ahmad <ayan.ahmad03600@gmail.com>
2025-11-10 19:28:57 +0530

    Add user prompts and confirmation logic in prompt.js

---
+++ prompt.js
@@ -0,0 +1,25 @@
+let name = prompt("What is your name?");
+console.log("Hello " + name + "!");
+
+
+What is your name? ➜ [User types "Ayan"]
+Console: Hello Ayan!
+
+
+let num = Number(prompt("Enter a number:"));
+console.log("Double is:", num * 2);
+
+
+let sure = confirm("Do you really want to delete this file?");
+if (sure) {
+  console.log("File deleted!");
+} else {
+  console.log("Action cancelled.");
+}
+
+let sure = confirm("Do you really want to delete this file?");
+if (sure) {
+  console.log("File deleted!");
+} else {
+  console.log("Action cancelled.");
+}
